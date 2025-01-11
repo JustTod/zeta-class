9800c1ae9dfabca2a1136dc1b0b100eac73b6315
JustTod <todsaporn@ddxtransformation.com>
2025-01-11 18:50:22 +0700

    init fizzbuzz

---
+++ src/fizzBuzz/fizzBuzz.spec.ts
@@ -0,0 +1,39 @@
+import  fizzBuzz from './fizzBuzz';
+
+describe('FizzBuzz', () => {
+  it('input 1 should get 1', () => {
+    // arrange
+    const input = 1;
+    const expected = "1";
+
+    // act
+    const actual = fizzBuzz(input);
+
+    // assert
+    expect(actual).toBe(expected)
+  });
+
+  it('input 2 should get 2', () => {
+    // arrange
+    const input = 2;
+    const expected = "2";
+
+    // act
+    const actual = fizzBuzz(input);
+
+    // assert
+    expect(actual).toBe(expected)
+  });
+
+  it('input 3 should get Fizz', () => {
+    // arrange
+    const input = 3;
+    const expected = "Fizz";
+
+    // act
+    const actual = fizzBuzz(input);
+
+    // assert
+    expect(actual).toBe(expected)
+  });
+});
---
+++ src/fizzBuzz/fizzBuzz.ts
@@ -0,0 +1,5 @@
+export default (num: number): string => {
+  if (num == 1) return '1';
+  if (num ==2 ) return '2';
+  return 'Fizz';
+};
