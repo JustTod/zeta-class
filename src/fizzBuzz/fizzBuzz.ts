712beb762e7c9a1c85561eb7cda68c35a8159e33
JustTod <todsaporn@ddxtransformation.com>
2025-01-16 16:07:53 +0700

    fix: handle BuzzBang case

--- src/fizzBuzz/fizzBuzz.ts
+++ src/fizzBuzz/fizzBuzz.ts
@@ -2,8 +2,9 @@ export default (num: number): string => {
   const isMultipleOfThree = num % 3 === 0;
   const isMultipleOfFive = num % 5 === 0;
   const isMultipleOfSeven = num % 7 === 0;
-  if (isMultipleOfFive && isMultipleOfThree) return 'FizzBuzz';
+  if (isMultipleOfThree && isMultipleOfFive) return 'FizzBuzz';
   if (isMultipleOfThree && isMultipleOfSeven) return 'FizzBang';
+  if (isMultipleOfFive && isMultipleOfSeven) return 'BuzzBang';
   if (isMultipleOfSeven) return 'Bang';
   if (isMultipleOfThree) return 'Fizz';
   if (isMultipleOfFive) return 'Buzz';
