fe7980a8e4b2b26101666647e0e6e70b74031239
JustTod <todsaporn@ddxtransformation.com>
2025-01-16 11:10:03 +0700

    test: add check 5 is Buzz

--- src/fizzBuzz/fizzBuzz.spec.ts
+++ src/fizzBuzz/fizzBuzz.spec.ts
@@ -4,36 +4,60 @@ describe('FizzBuzz', () => {
   it('input 1 should get 1', () => {
     // arrange
     const input = 1;
-    const expected = "1";
+    const expected = '1';
 
     // act
     const actual = fizzBuzz(input);
 
     // assert
-    expect(actual).toBe(expected)
+    expect(actual).toBe(expected);
   });
 
   it('input 2 should get 2', () => {
     // arrange
     const input = 2;
-    const expected = "2";
+    const expected = '2';
 
     // act
     const actual = fizzBuzz(input);
 
     // assert
-    expect(actual).toBe(expected)
+    expect(actual).toBe(expected);
   });
 
   it('input 3 should get Fizz', () => {
     // arrange
     const input = 3;
-    const expected = "Fizz";
+    const expected = 'Fizz';
 
     // act
     const actual = fizzBuzz(input);
 
     // assert
-    expect(actual).toBe(expected)
+    expect(actual).toBe(expected);
   });
+
+  it('input 4 should get 4', () => {
+    // arrange
+    const input = 4;
+    const expected = '4';
+
+    // act
+    const actual = fizzBuzz(input);
+
+    // assert
+    expect(actual).toBe(expected);
+  });
+
+  it('input 5 should get Buzz', () => {
+    // arrange
+    const input = 5;
+    const expected = 'Buzz';
+
+    // act
+    const actual = fizzBuzz(input);
+
+    // assert
+    expect(actual).toBe(expected);
+  })
 });
