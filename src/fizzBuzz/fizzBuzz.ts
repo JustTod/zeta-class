56e6d18ec498de0519678eb03c6a232326adb892
JustTod <todsaporn@ddxtransformation.com>
2025-01-16 13:43:08 +0700

    fix: handle Bang case

--- src/fizzBuzz/fizzBuzz.ts
+++ src/fizzBuzz/fizzBuzz.ts
@@ -1,7 +1,9 @@
 export default (num: number): string => {
   const isMultipleOfThree = num % 3 === 0;
   const isMultipleOfFive = num % 5 === 0;
+  const isMultipleOfSeven = num % 7 === 0;
   if (isMultipleOfFive && isMultipleOfThree) return 'FizzBuzz';
+  if (isMultipleOfSeven) return 'Bang';
   if (isMultipleOfThree) return 'Fizz';
   else if (isMultipleOfFive) return 'Buzz';
   else return num.toString();
