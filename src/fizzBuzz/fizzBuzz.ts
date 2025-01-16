57db9bb7f3be1b80e5f541a55735be36356bafd3
JustTod <todsaporn@ddxtransformation.com>
2025-01-16 12:36:25 +0700

    fix: handle FizzBuzz case

--- src/fizzBuzz/fizzBuzz.ts
+++ src/fizzBuzz/fizzBuzz.ts
@@ -1,7 +1,7 @@
 export default (num: number): string => {
   const isMultipleOfThree = num % 3 === 0;
   const isMultipleOfFive = num % 5 === 0;
-
+  if (isMultipleOfFive && isMultipleOfThree) return 'FizzBuzz';
   if (isMultipleOfThree) return 'Fizz';
   else if (isMultipleOfFive) return 'Buzz';
   else return num.toString();
