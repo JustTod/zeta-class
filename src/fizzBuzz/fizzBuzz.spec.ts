a628a21b2b2dfa816ed57217a9f62ae9769453eb
JustTod <todsaporn@ddxtransformation.com>
2025-01-16 12:00:46 +0700

    refactor: 3 test cases. regular, fizz and buzz

--- src/fizzBuzz/fizzBuzz.spec.ts
+++ src/fizzBuzz/fizzBuzz.spec.ts
@@ -1,87 +1,46 @@
 import  fizzBuzz from './fizzBuzz';
 
 describe('FizzBuzz', () => {
-  it('input 1 should get 1', () => {
-    // arrange
-    const input = 1;
-    const expected = '1';
+  it.each`
+    input | expected
+    ${1}  | ${'1'}
+    ${2}  | ${'2'}
+    `(
+      `should be $expected when is not match rule ($input)`,
+      ({ input, expected }: { input: number; expected: string }) => {
+        // Act
+        const actual = fizzBuzz(input);
+        
+        // Assert
+        expect(actual).toBe(expected);
+      });
 
-    // act
-    const actual = fizzBuzz(input);
+  it.each`
+    input
+    ${3}
+    ${6}
+    ${9}
+  `(
+    `should be Fizz when it is multiply by 3 ($input)`,
+    ({ input }: { input: number }) => {
+      // Act
+      const actual = fizzBuzz(input);
 
-    // assert
-    expect(actual).toBe(expected);
-  });
+      // Assert
+      expect(actual).toBe('Fizz');
+    });
 
-  it('input 2 should get 2', () => {
-    // arrange
-    const input = 2;
-    const expected = '2';
+  it.each`
+    input
+    ${5}
+    ${10}
+  `(
+    `should be Buzz when it is multiply by 5 ($input)`,
+    ({ input }: { input: number }) => {
+      // Act
+      const actual = fizzBuzz(input);
 
-    // act
-    const actual = fizzBuzz(input);
-
-    // assert
-    expect(actual).toBe(expected);
-  });
-
-  it('input 3 should get Fizz', () => {
-    // arrange
-    const input = 3;
-    const expected = 'Fizz';
-
-    // act
-    const actual = fizzBuzz(input);
-
-    // assert
-    expect(actual).toBe(expected);
-  });
-
-  it('input 4 should get 4', () => {
-    // arrange
-    const input = 4;
-    const expected = '4';
-
-    // act
-    const actual = fizzBuzz(input);
-
-    // assert
-    expect(actual).toBe(expected);
-  });
-
-  it('input 5 should get Buzz', () => {
-    // arrange
-    const input = 5;
-    const expected = 'Buzz';
-
-    // act
-    const actual = fizzBuzz(input);
-
-    // assert
-    expect(actual).toBe(expected);
-  })
-
-  it('input 6 should get Fizz', () => {
-    // arrange
-    const input = 6;
-    const expected = 'Fizz';
-
-    // act
-    const actual = fizzBuzz(input);
-
-    // assert
-    expect(actual).toBe(expected);
-  })
-
-  it('input 9 should get Fizz', () => {
-    // arrange
-    const input = 9;
-    const expected = 'Fizz';
-
-    // act
-    const actual = fizzBuzz(input);
-
-    // assert
-    expect(actual).toBe(expected);
-  })
+      // Assert
+      expect(actual).toBe('Buzz');
+    });
 });
